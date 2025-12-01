b5c98e2b0345ad4e1542dc0d9a422e7f8e0ba735
Falkiri <rdanielrodriguez02180@gmail.com>
2025-11-30 23:19:07 -0800

    Instructor Update
    getInfo added

--- src/main/java/com/cms/backend/Instructor.java
+++ src/main/java/com/cms/backend/Instructor.java
@@ -1,6 +1,11 @@
+<<<<<<< Updated upstream:src/main/java/com/cms/backend/Instructor.java
 package com.cms.backend;
 
 import java.util.*;
+=======
+import java.util.ArrayList;
+import java.util.List;
+>>>>>>> Stashed changes:src/main/java/com/cms/backend/Professor.java
 
 public class Instructor extends User {
     private List<Assignment> assignments = new ArrayList<>();
@@ -28,5 +33,10 @@ public class Instructor extends User {
 
     public void addCourse(Course course) {
         courses.add(course);
+    }
+
+    public String getInfo() {
+        return String.format("Professor: %s (%s) | Email: %s | Phone: %d | Courses: %d | Assignments: %d",
+                name, username, email, PhoneNumber, courses.size(), assignments.size());
     }
 }
